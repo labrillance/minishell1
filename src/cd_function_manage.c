a2393e7b3c3814a6f96a7cab91984136f63e56ba
loris.briere <loris.briere@epitech.eu>
2020-01-28 16:17:33 +0100

    adding cd - option

--- src/cd_function_manage.c
+++ src/cd_function_manage.c
@@ -26,6 +26,31 @@ char **change_oldpdw(char **env, int y)
     return env;
 }
 
+char *get_old_pdw(char **env)
+{
+    int i = 0;
+    int tmp = 0;
+    int x = 0;
+    int y = 0;
+    char *result = 0;
+
+    for (; tmp == 0; i++) {
+        if (env[i][0] == 'O' && env[i][1] == 'L' && env[i][2] == 'D' &&
+            env[i][3] == 'P' && env[i][4] == 'W' && env[i][5] == 'D' &&
+            env[i][6] == '=')
+            tmp = 1;
+    }
+    i--;
+    result = malloc(sizeof(char) * my_strlen(env[i]));
+    for (; env[i][x] != '='; x++);
+    x++;
+    for (; env[i][x] != 0; x++, y++)
+        result[y] = env[i][x];
+    result[y] = '/';
+    result[y+ 1] = 0;
+    return result;
+}
+
 char **change_pwdname(char **env)
 {
     int i = 0;
@@ -47,22 +72,24 @@ char **change_pwdname(char **env)
     return env;
 }
 
-char *cd_options(char **opt)
+char *cd_options(char **opt, char **env)
 {
     char *tmp = NULL;
     char *buf = NULL;
     size_t size = 1000;
 
-    if (opt[1] != 0 && opt[1][0] != '/' && opt[1][0] != '~') {
+    if (opt[1] != 0 && opt[1][0] != '/' && opt[1][0] != '~' && opt[1][0] != '-') {
         buf = getcwd(buf, size);
         buf = add_n(buf);
         tmp = malloc(sizeof(char) * my_strlen(buf) * 2);
         tmp[0] = 0;
         buf = my_strcat(buf, my_strcpy(tmp, opt[1]));
-    } else if (opt[1] != NULL && opt[1][0] != '~') {
+    } else if (opt[1] != NULL && opt[1][0] != '~' && opt[1][0] != '-') {
         tmp = malloc(sizeof(char) * my_strlen(opt[1]) * 2);
         tmp = my_strcpy(tmp, opt[1]);
     }
+    if (opt[1] != NULL && opt[1][0] == '-' && opt[1][1] == 0)
+        tmp = get_old_pdw(env);
     if (buf != NULL)
         tmp = my_strcpy(tmp, buf);
     free(buf);
@@ -76,7 +103,7 @@ int my_cd(char **opt, char *str, char **env)
     size_t size = 1000;
 
     if (str[0] == 'c' && str[1] == 'd' && str[2] == 0) {
-        tmp = cd_options(opt);
+        tmp = cd_options(opt, env);
         if (tmp != 0) {
             if (chdir(tmp) == -1)
                 perror("cd ");
